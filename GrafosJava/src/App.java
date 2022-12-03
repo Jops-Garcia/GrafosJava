8b998d4c3c063944dcd89e4ee59abe41c0d46d62
João Pedro Garcia Pereira <garcia.jops@gmail.com>
2022-12-03 15:55:16 -0300

    Adiciona alg de PRIM

--- GrafosJava/src/App.java
+++ GrafosJava/src/App.java
@@ -27,101 +27,113 @@ public class App {
         int codigoAux = 0;
 
         //MENU
-        while (opcao!=5) {            
+        while (opcao!=7) {            
             clear();
             System.out.print("##---------------------Menu-------------------##\n\n");
-            System.out.print("|------------------------------------------------|\n");
-            System.out.print("| Opção 1 - Exibir matriz e nós                  |\n");
-            System.out.print("| Opção 2 - Obter Nós vizinhos                   |\n");
-            System.out.print("| Opção 3 - Obter todos os caminhos              |\n"); 
-            System.out.print("| Opção 4 - Encontrar caminho minimo (dijkstra)  |\n"); //Deixando comentados para uso no futuro
-            System.out.print("| Opção 5 - Sair                                 |\n");
-            System.out.print("|------------------------------------------------|\n");
+            System.out.print("|-------------------------------------------------|\n");
+            System.out.print("| Opção 1 - Exibir matriz e nós                   |\n");
+            System.out.print("| Opção 2 - Obter Nós vizinhos                    |\n");
+            System.out.print("| Opção 3 - Obter todos os caminhos               |\n"); 
+            System.out.print("| Opção 4 - Encontrar caminho minimo (Dijkstra)   |\n"); 
+            System.out.print("| Opção 5 - Calcular Árvore geradora mínima (Prim)|\n");
+            System.out.print("| Opção 6 - Calcular Fluxo Máximo (Ford-Fulkerson)|\n");//Deixando comentados para uso no futuro
+            System.out.print("| Opção 7 - Sair                                  |\n");
+            System.out.print("|-------------------------------------------------|\n");
             System.out.print("Digite uma opção: ");
 
             opcao = menu.nextInt();
 
-            if (opcao == 5) {
+            if (opcao == 7) {
                 //Fechando os scanners
                 menu.close();
                 aux.close();
             }
 
             switch (opcao) {
-            case 1://Exibir matriz e nós
-                clear();
-                test.printNodes();
-                System.out.println();
-                test.printMatriz();
+                case 1://Exibir matriz e nós
+                    clear();
+                    test.printNodes();
+                    System.out.println();
+                    test.printMatriz();
                 break;
 
-            case 2://Obter Nós vizinhos
-                clear();
-                test.printNodes();
-                System.out.println(); //Prints para auxiliar o user
-                System.out.print("Digite o nó desejado: ");
-                codigo=aux.nextInt(); 
-                aux.nextLine();//lixo para coletar espaço
-                clear();
+                case 2://Obter Nós vizinhos
+                    clear();
+                    test.printNodes();
+                    System.out.println(); //Prints para auxiliar o user
+                    System.out.print("Digite o nó desejado: ");
+                    codigo=aux.nextInt(); 
+                    aux.nextLine();//lixo para coletar espaço
+                    clear();
+                    
+                    test.vizinhos(codigo-1);
+                break;
+
+                case 3://Obter todos os caminhos
+                    clear();
+                    test.printNodes();
+                    System.out.println();//Prints para auxiliar o user
+                    System.out.print("Digite o nó desejado: ");
+                    codigo=aux.nextInt(); 
+                    aux.nextLine();//lixo para coletar espaço
+                    clear();
+
+                    //Printando node selecionado e seu codigo
+                    System.out.println("Code: "+(codigo)+" Node: "+test.nodes.get(codigo-1));
+                    System.out.println("\nCaminhos:");
+                    
+                    //Criando auxiliar para nao passsar a array como referencia
+                    ArrayList<String> aux1=new ArrayList<String>(test.nodes); 
+                    test.caminhos(codigo-1,aux1);//-1 Um no código para acertar a posicao da cidade 
+                    System.out.println();
+                break;
+
+                case 4:
+                    clear();
+                    test.printNodes();
+                    System.out.println(); //Prints para auxiliar o user
+                    System.out.print("Digite o nó de origem desejado: ");
+                    codigo=aux.nextInt(); 
+                    aux.nextLine();//lixo para coletar espaço
+
+                    System.out.println(); //Prints para auxiliar o user
+                    System.out.print("Digite o nó de destino desejado: ");
+                    codigoAux=aux.nextInt(); 
+                    aux.nextLine();//lixo para coletar espaço
+                    clear();
+
+                    test.distanciaMinima(codigo-1,codigoAux-1);
+                break;
+
+                case 5:
+                    clear();
+                    test.primMST(test.matriz);
+                    
+                break;             
                 
-                test.vizinhos(codigo-1);
+                case 6:
+                    clear();
+                    
+                break;
+   
+                case 7:
+                    System.out.print("\nAté logo\n\n");
                 break;
 
-            case 3://Obter todos os caminhos
-                clear();
-                test.printNodes();
-                System.out.println();//Prints para auxiliar o user
-                System.out.print("Digite o nó desejado: ");
-                codigo=aux.nextInt(); 
-                aux.nextLine();//lixo para coletar espaço
-                clear();
-
-                //Printando node selecionado e seu codigo
-                System.out.println("Code: "+(codigo)+" Node: "+test.nodes.get(codigo-1));
-                System.out.println("\nCaminhos:");
-                
-                //Criando auxiliar para nao passsar a array como referencia
-                ArrayList<String> aux1=new ArrayList<String>(test.nodes); 
-                test.caminhos(codigo-1,aux1);//-1 Um no código para acertar a posicao da cidade 
-                System.out.println();
-                break;
-
-             //Deixando comentarios para uso no futuro
-             case 4:
-                clear();
-                test.printNodes();
-                System.out.println(); //Prints para auxiliar o user
-                System.out.print("Digite o nó de origem desejado: ");
-                codigo=aux.nextInt(); 
-                aux.nextLine();//lixo para coletar espaço
-
-                System.out.println(); //Prints para auxiliar o user
-                System.out.print("Digite o nó de destino desejado: ");
-                codigoAux=aux.nextInt(); 
-                aux.nextLine();//lixo para coletar espaço
-                clear();
-
-                test.distanciaMinima(codigo-1,codigoAux-1);
-
-
-                break;
-            case 5:
-                System.out.print("\nAté logo\n\n");
-                break;
-            default:
-                System.out.print("\nOpção Inválida!");
+                default:
+                    System.out.print("\nOpção Inválida!");
                 break;
             }
-            if(opcao!=5){
-            System.out.print("\nPressione Enter para continuar...");
-            aux.nextLine();
+            if(opcao!=7){
+                System.out.print("\nPressione Enter para continuar...");
+                aux.nextLine();
             }
         }
     }
     //Função para limpar tela
     public static void clear() throws IOException, InterruptedException{
         if (System.getProperty("os.name").contains("Windows")){//Pega o sistema operacional e utiliza o comando com base
-        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
         }
         else{
             Runtime.getRuntime().exec("clear");
--- GrafosJava/src/Grafos.java
+++ GrafosJava/src/Grafos.java
@@ -43,13 +43,11 @@ public class Grafos {
     }
 
     public void caminhos(int cidade,ArrayList<String> cidades){//A array serve para saber os nós que já foram percorridos
-        
         // if (cidades.size()<0){
         //     return;
         // }
         //Removendo a atual
         cidades.remove(nodes.get(cidade));
-        
         //Percorrer a linha da matriz
         for (int c = 0; c < matriz[cidade].length; c++)
         {
@@ -80,7 +78,6 @@ public class Grafos {
     }
 
     public void printNodes(){
-
         for(int i=0;i<this.nodes.size();i++){
             //Pegando o codigo do nó, seu nome e printando
             System.out.println("Code: "+(i+1)+" Node: "+this.nodes.get(i));
@@ -105,32 +102,23 @@ public class Grafos {
         boolean[] spt = new boolean[tamanho];
         float [] distancia = new float[tamanho];
         float INFINITO = Integer.MAX_VALUE;
-    
         //COLOCA TODAS AS DISTANCIAS COMO "INFINITO" (maior inteiro)
         for (int i = 0; i <tamanho ; i++) {
             distancia[i] = INFINITO;
         }
-    
         //INICIA OS VERTICE DE ORIGEM COMO 0
         distancia[origem] = 0;
-    
         //CRIA SPT
         for (int i = 0; i <tamanho ; i++) {
-    
             //PEGA O VERTICE COM A MENOR DISTANCIA
             int linha = caminhoMinVertice(spt, distancia);
-        
             //INCLUI ESSE VERTICE NO SPT
             spt[linha] = true;
-        
             //PERCORRE POR TODOS OS VERTICES E ATUALIZA A KEY
             for (int coluna = 0; coluna <tamanho ; coluna++) {
-                if(matriz[linha][coluna]>0){
-                
+                if(matriz[linha][coluna]>0){            
                     if(spt[coluna]==false && matriz[linha][coluna]!=INFINITO){
                         //VERIFICA SE A DISTANCIA PRECISA ATUALIZAR OU NAO, VE SE O A DISTANCIA DA CIDADE DE ORIGEM É MENOR QUE A DISTANCIA ATUAL E SE SIM ATUALIZA
-
-                    
                         float newKey = matriz[linha][coluna] + distancia[linha];
                         if(newKey<distancia[coluna])
                         distancia[coluna] = newKey;
@@ -150,5 +138,84 @@ public class Grafos {
                 " distancia: " + key[i]);
             }
         }
+    }
+
+    //Prim +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    
+    //Funcao para achar o vertice com a key
+    //MST = Minimum Spanning Tree (ARRVORE GERADORA MINIMA)
+    int minKey(float key[], Boolean mstSet[])
+    {
+        // inicializa com o maior e menor valor possivel
+        float min = Float.MAX_VALUE;
+        int min_index = -1;
+ 
+        for (int v = 0; v < tamanho; v++)
+            if (mstSet[v] == false && key[v] < min) {
+                min = key[v];
+                min_index = v;
+            }
+ 
+        return min_index;
+    }
+ 
+    //Funcao para printar a MST
+
+    void printMST(int mst[], float grafo[][])
+    {
+        System.out.println("ORIGEM\t          DESTINO\tPESO\n");
+        float pesoTotal =0;
+        for (int i = 1; i < tamanho; i++){
+            System.out.println(this.nodes.get(mst[i])+ "(" + (mst[i]+1) +")     \t"  +this.nodes.get(i)+"(" + (i+1) +")\t" + grafo[i][mst[i]]);
+            pesoTotal = pesoTotal+grafo[i][mst[i]];      
+        }
+        System.out.println("\nPeso total: " + pesoTotal);
+    }
+ 
+    //Funcao para construir e printar a MST de um grafo
+    void primMST( float grafo[][])
+    {
+        //Arrray para guardar a MST
+        int mst[] = new int[tamanho];
+ 
+        //Valores para pegar o peso minimo nas arestas
+        float key[] = new float[tamanho];
+ 
+        //vertices incluidos na MST
+        Boolean mstSet[] = new Boolean[tamanho];
+ 
+        //inicializando todos os valores como infinito
+        for (int i = 0; i < tamanho; i++) {
+            key[i] = Integer.MAX_VALUE;
+            mstSet[i] = false;
+        }
+ 
+        //Inclui o primeiro vertice no mst
+        key[0] = 0;
+        mst[0] = -1; // primeiro nó inserido e como raiz
+ 
+        // A arvore geradora minima vai ter o mesmo tamanho que o grafo, entao..
+        for (int count = 0; count < tamanho - 1; count++) {
+            //pega o valor minimo do vertice pelo mstSet ainda nao incluido
+            int u = minKey(key, mstSet);
+ 
+            //adiciona o vertice selecionado como ja visitado
+            mstSet[u] = true;
+ 
+
+            //Atualiza o valor e o mst dos vertices adjacentes do escolhido, só considera os que não estao incluidos na mstSet
+            for (int v = 0; v < tamanho; v++)
+ 
+                // grafo[u][v] nao é zero só para os vertices adjacentes 
+                // mstSet[v] é falso para vertices nao inseridos na MST
+                // só atualiza key se grafo[u][v] é menor que key[v]
+                if (grafo[u][v] != 0 && mstSet[v] == false && grafo[u][v] < key[v]) {
+                    mst[v] = u;
+                    key[v] = grafo[u][v];
+                }
+        }
+ 
+        // printa a MST
+        printMST(mst, grafo);
     }
 }
